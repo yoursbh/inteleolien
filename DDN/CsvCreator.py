5ebc3eddb7916c1dff8568236485783a573a5e3d
Hao BAI <hao.bai@insa-rouen.fr>
2019-03-12 11:34:03 +0100

    HB : [DNN] Improve the network to predict the damages over 36 angles on gage 1; Add a Python script for pre-processing the data

---
+++ DDN/CsvCreator.py
@@ -0,0 +1,215 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# NAME OF SCRIPT
+#
+#
+# Authors: W. Veloz Parra, H. BAI
+# Date: 13/11/2018
+#
+# Version:
+#   - 0.0: Initial version
+#   - 0.1: export all 36 angles on tower gage 1
+# Comments:
+# 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+#!------------------------------------------------------------------------------
+#!                                          MODULES PRÉREQUIS
+#!------------------------------------------------------------------------------
+#*============================ Modules Personnels ============================
+from tools import utils
+#*============================== Modules Communs ==============================
+import sys
+import itertools
+import csv
+import numpy
+import collections
+import multiprocessing
+from os import listdir
+from os.path import isfile, join
+from itertools import islice
+from pathlib import Path
+import json
+
+#!------------------------------------------------------------------------------
+#!                                         CLASS
+#!------------------------------------------------------------------------------
+class CsvCreator(object):
+    ''' Description of this class
+        Attributes:
+            filebase: basename of .out file (i.e. name without extension) [str]
+            header: line number that contains column's name in input file [int]
+            datarow: line number that contains the first row of data in input file [int]
+            startline: begin writting results from this row in output file [int]
+            channels: channel names that will be used for searching [list]            
+        Methods:
+            find(): search peak/valley for a given channel name
+            run(): batch profile for finding processing
+    '''
+    def __init__(self, directory, wndfn, dmgfn, outputfile, gage, echo):
+        self.dir = directory
+        self.wndf = wndfn
+        self.dmgfn = dmgfn
+        self.outputfile = outputfile
+        self.gage = str(gage)
+        self.echo = echo
+        self.csvfile = []
+
+    def windcsv(self, echo=True):
+        t = 0
+        # sline = 6
+        muline = 129
+        mustop = 145
+        sigmaustep = 73
+        sigmavstep = 106
+        sigmawstep = 139
+        for f in self.wndf:
+            seed = []
+            muwind = []
+            sigmawind = []
+            fdata = self.file_read(t, f, extension=".sum")
+            # print(fdata)
+            flist = f.split("_")
+            # seed.append(int(flist[-1].rstrip(".sum")))
+            seed.append(int(flist[-1].rstrip(".sum")))
+            # seed.append(float(fdata[sline][0]))
+            for k in range(muline, mustop):
+                muaux = [float(fdata[k][j])for j in range(len(fdata[k])) if j == 4]
+                muwind.append(muaux)
+                sigmauaux = [float(fdata[k + sigmaustep][j])for j in range(len(fdata[k + sigmaustep])) if j == 16]
+                sigmavaux = [float(fdata[k + sigmavstep][j])for j in range(len(fdata[k + sigmavstep])) if j == 16]
+                sigmawaux = [float(fdata[k + sigmawstep][j])for j in range(len(fdata[k + sigmawstep])) if j == 16]
+                sigmawind.append(list(itertools.chain(*[sigmauaux, sigmavaux, sigmawaux])))
+            csvf = []
+            csvf.append(list([seed]))
+            csvf.append(muwind)
+            csvf.append(sigmawind)
+            csvf = list(itertools.chain(*csvf))
+            csvf = list(itertools.chain(*csvf))
+            self.csvfile.append(csvf)
+            # print(len(self.csvfile[0]))
+            # print(self.csvfile)
+            # break
+        pass
+
+    def damagecsv(self, echo=True):
+        j = 0
+        for elem in self.csvfile:
+            seed = elem[0]
+            for damfile in self.dmgfn:
+                if str(seed) in damfile:
+                    with open(self.dir[-1] + damfile + ".dam","r") as f:
+                        damages = json.loads(f.read())
+                    outputline = []
+                    for theta in range(0, 360, 10):
+                        key = "TwHt{}@{}".format(self.gage, theta)
+                        outputline.append(damages[key]["Dcumulate"]*1e0) #1e9
+                        # outputline = [x*10e9 for x in outputline]
+                    # print(outputline)
+                    if j < len(self.csvfile):
+                        self.csvfile[j].extend(outputline)
+                        j = j + 1
+                    # print(self.csvfile[0])
+        # print(self.csvfile)
+        pass
+
+    def csvwrite(self, echo= True):
+        with open(self.outputfile, 'w') as sfile:
+            wr = csv.writer(sfile, dialect='excel', delimiter = ',')
+            wr.writerows(self.csvfile)
+
+    def file_read(self, t, filename, extension):
+        fdata = []
+        path = self.dir[t] + filename + extension
+        with open(path, 'r') as f:
+            for line in islice(f, 0, None):
+                data = line.split(None)
+                fdata.append(data)
+        return fdata
+
+#!------------------------------------------------------------------------------
+#!                                      FUNCTIONS
+#!------------------------------------------------------------------------------
+def testHao():
+    with utils.cd("/home/wilsonveloz/Documents/Hao_Data/Wind"):
+        # data = utils.readcsv("NTM_11mps_-1029744777.sum", delimiter="\s", header=112, datarow=130, endrow=145, ncols=2)
+        # print(data)
+        pass
+
+    # # 1st approch: forward
+    # with utils.cd('~/Eolien/Parameters/NREL_5MW_Onshore/Wind'):
+    #     with open('100seeds.json', 'r') as f:
+    #         seeds = json.loads(f.read())
+    # liste = [s for s in seeds if s[0] == "NTM" and s[1] == "13"]
+    # seeds = liste
+
+    # s = seeds[0]
+    # fname = "{}_{}mps_{}.sum".format(s[0], s[1], s[2])
+    
+    
+    # 2nd approch: backward
+    # fname # string of filename
+    # flist = fname.split("_")
+    # s = flist[-1].rstrip(".sum")
+
+    # for elem in wind_input_data:
+    #     seed = elem[0]
+    #     for damfile in damfilenames:
+    #         if seed in damfile:
+    #             with open(damfile,"r") as f:
+    #                 damages = json.loads(f.read())
+    #             outputline = []
+    #             for i in range(10,1,-1)
+    #                 key = "TwHt{}@0".format(i)
+    #                 outputline.append(damages[key]["Dcumulate"])
+
+    #     elem.extend(outputline)
+
+def find(path, pattern, size=None):
+    ''' size: minimum size in bytes (1 GB = 1024 MB = 1024^2 KB = 1024^3 Bytes) [num]
+    '''
+    with utils.cd(path):
+        p = Path().expanduser()
+        matched = sorted(p.glob(pattern))
+        if size is None:
+            result = [x.stem.split('.')[0] for x in matched]
+        else:
+            result = [x.stem.split('.')[0]
+                        for x in matched if x.stat().st_size >= size]
+    return result
+
+
+#!------------------------------------------------------------------------------
+#!                                     MAIN FUNCTION
+#!------------------------------------------------------------------------------
+def main():
+    # testHao()
+    # return
+    windpath = "./data/"
+    damagepath = "./data/"
+    directory = [windpath, damagepath]
+    
+    # windfiles = [f for f in listdir(windpath) if isfile(join(windpath, f))]
+    # damagefiles = [f for f in listdir(damagepath) if isfile(join(damagepath, f))]
+    windfiles = find(windpath, "*.sum")
+    damagefiles = find(damagepath, "*.dam")
+
+
+    DmgG1 = CsvCreator(directory=directory, wndfn= windfiles,
+                       dmgfn= damagefiles, outputfile="DataNS1000_gage1.csv", gage="1",
+                       echo=True)
+    DmgG1.windcsv()
+    DmgG1.damagecsv()
+    DmgG1.csvwrite()
+
+
+
+#!------------------------------------------------------------------------------
+#!                                         EXÉCUTION
+#!------------------------------------------------------------------------------
+if __name__ == '__main__':
+    main()
--- DDN/dnn_dmg_1000_gage1.py
+++ DDN/dnn_dmg_1000_gage1.py
@@ -11,22 +11,26 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow as tf
-# import seaborn as sns
+try:
+    import seaborn as sns
+except:
+    pass
 from sklearn.preprocessing import StandardScaler
 
 
-# HB : test modification
-
-# Data loading: Damage for 100 simulation
+# Data loading: Damage for 1000 simulation
 Data = pd.read_csv('./DataNS1000_gage1.csv')
 Data
 
 # Data selection:
 wind = Data.values[:,1:65]
-dmg = Data.values[:,65]
+dmg = Data.values[:,65:]
+#dmg = Data.values[:,65]
 XX = wind
 m, n = XX.shape
 yy = dmg
+p, q = yy.shape
+#q = 1
 
 # Scaling Data
 scaler = StandardScaler()
@@ -34,16 +38,17 @@ XX = scaler.fit_transform(XX)
 
 # Train and Test set
 X_train = XX[:10000].reshape(-1,n)
-y_train = yy[:10000].reshape(-1,1)
+y_train = yy[:10000].reshape(-1,q)
 X_test = XX[10000:].reshape(-1,n)
-y_test = yy[10000:].reshape(-1,1)
+y_test = yy[10000:].reshape(-1,q)
 
 m,n = X_train.shape
-p = y_train.shape
+p, q = y_train.shape
+#p = y_train.shape
 
-Labels = ['W-14','W-13','Sgm-u-13','Sgm-v-13','Sgm-w-13','Sgm-u-14','Sgm-v-14','Sgm-w-14','Damage']
-Dataf = pd.DataFrame({Labels[0]:XX[:,0],Labels[1]:XX[:,1],Labels[2]:XX[:,2],Labels[3]:XX[:,3],Labels[4]:XX[:,4],Labels[5]:XX[:,5],Labels[6]:XX[:,6],Labels[7]:XX[:,7],Labels[8]:yy})
-# sns.pairplot(Dataf, diag_kind="kde")
+#Labels = ['W-14','W-13','Sgm-u-13','Sgm-v-13','Sgm-w-13','Sgm-u-14','Sgm-v-14','Sgm-w-14','Damage']
+#Dataf = pd.DataFrame({Labels[0]:XX[:,0],Labels[1]:XX[:,1],Labels[2]:XX[:,2],Labels[3]:XX[:,3],Labels[4]:XX[:,4],Labels[5]:XX[:,5],Labels[6]:XX[:,6],Labels[7]:XX[:,7],Labels[8]:yy})
+#sns.pairplot(Dataf, diag_kind="kde")
 
 # DNN Creation
 
@@ -66,11 +71,13 @@ def selu (z, scale=1.05070098735548, alpha=1.6732632423543772848):
 def fetch_batch(epoch, batch_index, batch_size):
     np.random.seed(epoch * n_batches + batch_index)  # not shown in the book
     indices = np.random.randint(m, size=batch_size)  # not shown
+    indices_y = np.random.randint(p, size=batch_size)
+    #indices_y = np.random.randint(p[0], size=batch_size)
     X_batch = X_train.reshape(-1,n_input)[indices] # not shown
-    y_batch = y_train.reshape(-1,n_output)[indices] # not shown
+    y_batch = y_train.reshape(-1,n_output)[indices_y] # not shown
     return X_batch, y_batch
   
-n_epoch = 100
+n_epoch = 200
 learning_rate = 0.001
 
 n_input = n
@@ -81,7 +88,7 @@ n_hidden4 = 200
 n_hidden5 = 200
 n_hidden6 = 100
 n_hidden7 = 100
-n_output = 1
+n_output = q
 
 X = tf.placeholder(tf.float32, shape=[None, n_input], name='X')
 y = tf.placeholder(tf.float32, shape=[None, n_output], name='y')
@@ -143,9 +150,8 @@ mse_train = []
 mse_test = []
 
 with tf.Session() as sess:
-#   sess.run(init)
-  saver.restore(sess, "Damage100.ckpt")  # load best model
-  
+  sess.run(init)
+  saver.restore(sess, "./Damage100.ckpt")
   for epoch in range(n_epoch):
     for batch_index in range(n_batches):
       X_b, y_b = fetch_batch(epoch, batch_index, batch_size)
@@ -165,16 +171,30 @@ plt.plot(mse_test, label='MSE test')
 plt.legend()
 plt.show()
 
-plt.scatter(range(len(acc_test)),acc_test)
+plt.scatter(range(len(acc_test[:,12])),acc_test[:,12])
+plt.xlabel("No. Sample")
+plt.ylabel("Absolute Error: Output Prediction - Output")
 
-tol = 0.002
-correct_test = [1 for i in acc_test if i < tol]
-total_correct = len(correct_test)/len(acc_test)
+tol = 0.001
+correct_test = [1 for i in acc_test[:,12] if i < tol]
+total_correct = len(correct_test)/len(acc_test[:,12])
 total_correct
 
-plt.hist(y_test,bins=50)
+plt.hist(y_test[:,12],bins=50)
+plt.ylabel('Frequency')
+plt.xlabel('Damage')
 plt.show()
 
-plt.hist(y_pred,bins=50)
+plt.hist(y_pred[:,12],bins=50)
+#plt.xlim([-0.0002,0.0002])
+plt.ylabel('Frequency')
+plt.xlabel('Damage Predicted')
 plt.show()
 
+y_pred[:,0]
+
+y_test[:,0]
+
+X_test_dt = scaler.inverse_transform(X_test)
+
+X_test_dt[:,3]
